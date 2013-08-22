e8ed767c8acf731c6470d843273fe24366e8d90b
JunOu <junmse.ou@gmail.com>
2013-08-21 23:14:24 -0700

    compiled for windows exe

--- source/setup.py
+++ source/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 import py2exe
 
-options = {"py2exe": {'dist_dir': "../dist"},
+options = {"py2exe": {"packages": ['wx.lib.floatcanvas.NavCanvas','wx.lib.floatcanvas.FloatCanvas'],"dist_dir": "../dist"},
            'build': {'build_base': '../build'}}
            
 setup(windows=[{'script': 'CoordXY.py',
